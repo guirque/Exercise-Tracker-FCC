abbc61fdc8a9dbc264f5d5b36f00dc40b1e963aa
Guilherme <guirque@gmail.com>
2023-01-15 18:44:49 -0300

    Adding Date Parser
    A function that handles the text to date object conversion. It also returns a date object with the current date in case the one provided isn't in the appropriate format

---
+++ dateParser.js
@@ -0,0 +1,15 @@
+//Function to parse the date received
+function parseDate(date)
+{
+    //Behaviour: if the date isn't in the appropriate format, the current date will be used instead.
+    console.log(`Parsing ${date}`);
+    if(date.match(/\d\d\d\d-\d\d-\d\d/) !== null && date.length === 10) 
+    {
+        let [year, month, day] = date.match(/[^-]+/ig);
+        if(month != '00') month = `${Number(month) - 1}`; //fixing month, since january is month 0
+        return new Date(year, month, day);   
+    }
+    else return new Date();
+}
+
+module.exports = {parseDate};
